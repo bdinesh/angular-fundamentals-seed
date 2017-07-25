b010113b677cfd97f017b2dc8671ac6c672e89fe
dinesh bogolu <visitdinu@gmail.com>
2017-07-25 06:33:37 +0530

    Implement data bindings
    - Implement one-way and two-way bindings using FormsModule
    - Implement template ref, we can use template ref (#ref) to a dom element and
    access its values anywhere in the template.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,35 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      {{title}}
+      <div>
+        <input id="firstname" [ngModel]="name" (ngModelChange)="handleChange($event)"/>
+        <input id="lastname" [(ngModel)]="name"/>
+      </div>
+    </div>
+    <div>
+      <input #mobile>
+      <button (click)="handleClick(mobile.value)">Get Value</button>
+      <label>{{ mobileNumber }}</label>
     </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  title: string;
+  name: string = 'Dinesh';
+  mobileNumber: string;
+
+  constructor() {
+    this.title = 'Learning Angular!';
+  }
+
+  handleChange(value: string) {
+    this.name = value;
+  }
+
+  handleClick(value:  string) {
+    console.log(value);
+    this.mobileNumber = value;
+  }
 }
