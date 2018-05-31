bac6b78c5dedc37ee5382c80fe041c462ad155a6
Dinesh Bogolu <visitdinu@gmail.com>
2018-05-31 22:39:24 +0530

    Add home and not-found components

---
+++ app/not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'not-found',
+  template: `
+    <div>
+      Route not found... Please check your URL!
+    </div>
+  `
+})
+export class NotFoundComponent {}
