23552566441a05a28201ff072f46ca5afd4d8aa8
Dinesh Bogolu <visitdinu@gmail.com>
2018-06-06 12:58:11 +0530

    Use Renderer2 to set element attributes

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,9 +1,25 @@
-import {Component, Output, EventEmitter, ContentChild, AfterContentInit} from "@angular/core";
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ContentChild,
+  AfterContentInit,
+  AfterViewInit,
+  ElementRef,
+  Renderer2,
+  ViewChild
+} from "@angular/core";
 
 import {AuthRememberComponent} from "./auth-remember.component";
 
 @Component({
   selector: 'auth-form',
+  styles: [`
+    .email{
+      border-color: #9f72e6;
+      border-radius: 5px;
+    }
+  `],
   template:`
     <form #form="ngForm" (ngSubmit)="onSubmit(form.value)">
       <ng-content select="h3"></ng-content>
@@ -23,8 +39,10 @@ import {AuthRememberComponent} from "./auth-remember.component";
     </form>
   `
 })
-export class AuthFormComponent implements AfterContentInit{
+export class AuthFormComponent implements AfterContentInit, AfterViewInit{
   showMessage: boolean = false;
+
+  @ViewChild('email') email: ElementRef;
 
   @ContentChild(AuthRememberComponent)
   authRemember: AuthRememberComponent;
@@ -32,10 +50,18 @@ export class AuthFormComponent implements AfterContentInit{
   @Output()
   submitted: EventEmitter<any> = new EventEmitter<any>();
 
+  constructor(private renderer: Renderer2){}
+
   ngAfterContentInit(): void {
     if(this.authRemember){
       this.authRemember.checked.subscribe((data: boolean) => this.showMessage=data);
     }
+  }
+
+  ngAfterViewInit(): void {
+    // Using Renderer2 to set the attributes of elements
+    this.renderer.setAttribute(this.email.nativeElement, 'placeholder', 'Enter your email');
+    this.renderer.addClass(this.email.nativeElement, 'email');
   }
 
   onSubmit(value){
